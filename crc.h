a02358079d47eaa5c290b74291aea3765409b804
James Ren <qren@temple.edu>
2018-10-20 11:40:02 -0500

    Update crc.h

--- crc.h
+++ crc.h
@@ -7,38 +7,38 @@
 
 #if defined(CRC_CCITT)
 
-typedef unsigned short  crc;
+typedef unsigned short      crc;
 
 //CRC-CCITT=X16+X12+X5+X0
 
-#define POLYNOMIAL			  0x1021
-#define INITIAL_REMAINDER	0xFFFF
-#define FINAL_XOR_VALUE		0x0000
+#define POLYNOMIAL			    0x1021
+#define INITIAL_REMAINDER	  0xFFFF
+#define FINAL_XOR_VALUE		  0x0000
 
 #elif defined(CRC16)
 
-typedef unsigned short    crc;
+typedef unsigned short      crc;
 
 //CRC16=X16+X15+X2+X0
-#define POLYNOMIAL			  0x8005
-#define INITIAL_REMAINDER	0x0000
-#define FINAL_XOR_VALUE		0x0000
+#define POLYNOMIAL			    0x8005
+#define INITIAL_REMAINDER	  0x0000
+#define FINAL_XOR_VALUE		  0x0000
 
 #elif defined(CRC32)
 
-typedef unsigned long     crc;
+typedef unsigned long       crc;
 
 //CRC32=X32+X26+X23+X22+X16+X12+X11+X10+X8+X7+X5+X4+X2+X1+X0
-#define POLYNOMIAL			  0x04C11DB7
-#define INITIAL_REMAINDER	0xFFFFFFFF
-#define FINAL_XOR_VALUE		0xFFFFFFFF
+#define POLYNOMIAL			    0x04C11DB7
+#define INITIAL_REMAINDER	  0xFFFFFFFF
+#define FINAL_XOR_VALUE		  0xFFFFFFFF
 
 #endif
 
 void  crcTableGen(void);
 
 crc crcCompute(const unsigned char * msg, const unsigned int nBytes);
-unsigned short crc16(const char * msg, int len);
-unsigned long crc32(const char * msg, int len);
+//unsigned short crc16(const char * msg, int len);
+//unsigned long crc32(const char * msg, int len);
 
 #endif /* _crc_h */
